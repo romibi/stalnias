73d7560612ceff2e71d3138343e91cd1d1f7c1ec
romibi <romibi@bluewin.ch>
2017-02-14 11:57:03 +0100

    First Tile without Texture

---
+++ Assets/TileMap.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class TileMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BuildTileMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void BuildTileMap() {
+        Vector3[] vertices = new Vector3[4];
+        int[] triangles = new int[2 * 3];
+        Vector3[] normals = new Vector3[4];
+
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(0, 1, 0);
+        vertices[2] = new Vector3(1, 1, 0);
+        vertices[3] = new Vector3(1, 0, 0);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 3;
+
+        normals[0] = Vector3.forward;
+        normals[1] = Vector3.forward;
+        normals[2] = Vector3.forward;
+        normals[3] = Vector3.forward;
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+
+        MeshFilter mesh_filter = GetComponent<MeshFilter>();
+        MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
+        MeshCollider mesh_collider = GetComponent<MeshCollider>();
+
+        mesh_filter.mesh = mesh;
+    }
+}
