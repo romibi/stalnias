3c6c2eebd056fad519a8fb2dc02b15f53701f7a0
romibi <romibi@bluewin.ch>
2017-02-14 13:53:07 +0100

    TileMap with Size properties and Editor View

---
+++ Assets/Editor/TileMapInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapInspector : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Rebuild")) {
+            TileMap tilemap = (TileMap)target;
+            tilemap.BuildTileMap();
+        }
+    }
+}
--- Assets/TileMap.cs
+++ Assets/TileMap.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 public class TileMap : MonoBehaviour {
+    public int size_x = 20;
+    public int size_y = 10;
+    public float tileSize = 1f; 
 
 	// Use this for initialization
 	void Start () {
@@ -17,33 +21,48 @@ public class TileMap : MonoBehaviour {
 		
 	}
 
-    void BuildTileMap() {
-        Vector3[] vertices = new Vector3[4];
-        int[] triangles = new int[2 * 3];
-        Vector3[] normals = new Vector3[4];
+    public void BuildTileMap() {
+        int numTiles = size_x * size_y;
 
-        vertices[0] = new Vector3(0, 0, 0);
-        vertices[1] = new Vector3(0, 1, 0);
-        vertices[2] = new Vector3(1, 1, 0);
-        vertices[3] = new Vector3(1, 0, 0);
+        int vsize_x = size_x + 1;
+        int vsize_y = size_y + 1;
+        int numVertices = vsize_x * vsize_y;
 
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
+        Vector3[] vertices = new Vector3[numVertices];
+        Vector3[] normals = new Vector3[numVertices];
+        Vector2[] uv = new Vector2[numVertices];
 
-        triangles[3] = 0;
-        triangles[4] = 2;
-        triangles[5] = 3;
+        int[] triangleVertices = new int[numTiles * 2 * 3]; // numTiles * numTrianglesPerTile * numVerticesPerTriangle
 
-        normals[0] = Vector3.forward;
-        normals[1] = Vector3.forward;
-        normals[2] = Vector3.forward;
-        normals[3] = Vector3.forward;
+
+        for(int y=0; y < vsize_y; y++) {
+            for(int x=0; x < vsize_x; x++) {
+                int verticeIndex = y * vsize_x + x;     // verticeRow * rowSize + verticeInRow
+                vertices[verticeIndex] = new Vector3(x * tileSize, y * tileSize);
+                normals[verticeIndex] = Vector3.forward;
+                uv[verticeIndex] = new Vector2((float)x / vsize_x, (float)y / vsize_y);
+            }
+        }
+
+        for(int y=0; y < size_y; y++) {
+            for(int x=0; x < size_x; x++) {
+                int firstTriangleEdgeOfTile = (y * size_x + x) * 2 * 3;     // (tileRow * tileRowSize + tileInRow) * numTrianglesPerTile * numVerticesPerTriangle
+                int firstVerticeOfTile = (y * vsize_x + x);
+                triangleVertices[firstTriangleEdgeOfTile + 0] = firstVerticeOfTile;                     // Bottom Left
+                triangleVertices[firstTriangleEdgeOfTile + 1] = firstVerticeOfTile + vsize_x;           // Top Left (1 row higher)
+                triangleVertices[firstTriangleEdgeOfTile + 2] = firstVerticeOfTile + vsize_x + 1;       // Top Right (1 row higher & 1 vertice right)
+
+                triangleVertices[firstTriangleEdgeOfTile + 3] = firstVerticeOfTile;                // Bottom Left
+                triangleVertices[firstTriangleEdgeOfTile + 4] = firstVerticeOfTile + vsize_x + 1;  // Top Right (1 row higher & 1 vertice right)
+                triangleVertices[firstTriangleEdgeOfTile + 5] = firstVerticeOfTile + 1;            // Bottom Right (1 Vertice to the right)
+            }
+        }
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        mesh.triangles = triangleVertices;
         mesh.normals = normals;
+        mesh.uv = uv;
 
         MeshFilter mesh_filter = GetComponent<MeshFilter>();
         MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
