7be38d57ec9f4d17ee9278981b3ba80633338b3c
romibi <romibi@bluewin.ch>
2018-02-26 16:55:42 +0100

    Fix Camera size

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -18,7 +18,12 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        cam.orthographicSize = (Screen.height / 100f) / 4f;
+        // cam.orthographicSize = 1 -> in screen height are 2 Unity units
+        // cam.orthographicSize = 2 -> in screen height are 4 Unity units
+        // size = (screen.height / (2*pixelsPerUnit)) / scaleFactor
+        // 1 = (64 / (2*32)) / 1
+        // 2 = (128 / 64) / 1
+        cam.orthographicSize = (Screen.height / 64f) / 2f;
         if(target) {
             transform.position = Vector3.Lerp(transform.position, target.position+camOffset, 0.1f);
         }
